40bc6c2bc87d63f32885effe3a8948a1b6f27de4
nigga <fjin9911@gmail.com>
2025-11-15 16:33:49 +0300

    add smooth camera

--- godotFiles/test/player.gd
+++ godotFiles/test/player.gd
@@ -1,9 +1,21 @@
 extends CharacterBody3D
 
+#@onready var camera = $springArmSmooth
 
 const SPEED = 3.0
 const JUMP_VELOCITY = 4.5
+const MOUSE_SENS = 0.5
 
+
+func _ready():
+	pass
+
+func _input(event):
+	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("exit"):
+		get_tree().quit()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,8 +28,11 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	direction = direction.rotated(Vector3.UP, $springArmSmooth.global_rotation.y)
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -26,6 +41,3 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
-
-	#Make camera controller match the position of myself
-	$cameraController.position = lerp($cameraController.position, position, 0.1)
---
+++ godotFiles/test/spring_arm_camera.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+const MOUSE_SENS = 0.5
+@onready var spring_arm := $SpringArm3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * MOUSE_SENS
+		rotation.y = wrapf(rotation.y, 0.0, TAU)
+		
+		rotation_degrees.x -= event.relative.y * MOUSE_SENS
+		rotation.x = clamp(rotation.x, -PI/2, PI/4)
+
+	if event.is_action_pressed("wheel_up"):
+		spring_arm.spring_length -= 1
+	if event.is_action_pressed("wheel_down"):
+		spring_arm.spring_length += 1
