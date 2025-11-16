32ebbffcf89fb95d2cdb411ed0d1016e48df2599
nigga <fjin9911@gmail.com>
2025-11-16 12:49:15 +0300

    trying out blender

--- godotFiles/test/player.gd
+++ godotFiles/test/player.gd
@@ -6,17 +6,22 @@ const SPEED = 3.0
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENS = 0.5
 
+var xform : Transform3D
 
+# processes things only once when entering the scene
 func _ready():
 	pass
 
+# makes it do what you write in there once it detects input
 func _input(event):
 	pass
 
+# this one is making your game close when you hit esc
 func _process(delta):
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
 
+# processes the things written in there 60 times a second
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -31,13 +36,29 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forwards", "move_backwards")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
+	# makes you go where you look basically
 	direction = direction.rotated(Vector3.UP, $springArmSmooth.global_rotation.y)
 	
+# align the character to the floor
+	if is_on_floor():
+		align_with_floor($RayCast3D.get_collision_normal())
+		global_transform = global_transform.interpolate_with(xform, 0.3)
+	elif not is_on_floor():
+		align_with_floor(Vector3.UP)
+		global_transform = global_transform.interpolate_with(xform, 0.3)
+	
+	#makes your character move with certain speed
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+#this one speaks for itself
 	move_and_slide()
+
+func align_with_floor(floor_normal):
+	xform = global_transform
+	xform.basis.y = floor_normal
+	xform.basis.x = -xform.basis.z.cross(floor_normal)
+	xform.basis = xform.basis.orthonormalized()
